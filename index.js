f44bfdf7c32d53044b277221aa9950c2472ba34b
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-15 14:06:06 -0400

    logger passed testing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const chalk = require('chalk');
 const _ = require('lodash');
-const log = console.log;
 
-_.each([chalk.red, chalk.green, chalk.blue], function(x) {
-  log(x("Hello World!"));
-});
+const logger = require('./lib/logger.js');
+
+
+_.each([{ 'a':'Hello World', 'b':'INFO' }, { 'a':'Hello World', 'b':'WARNING' }, { 'a':'Hello World', 'b':'ERROR' }], function (x) {
+  logger(x.a, x.b);
+})
+
+
